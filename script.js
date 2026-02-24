d5c57d8c6e0cf3f8182ee672df49f95842dc92c8
anukriti-1209 <anukriti.25b01011209@abes.ac.in>
2026-02-24 09:45:56 +0530

    datatypes and js data values types

--- script.js
+++ script.js
@@ -1,2 +1,39 @@
-console.log("Hello World");
-document.write("hello world");
+
+var num=10;
+console.log(num);
+console.log(typeof(num));
+num=false;
+console.log(num);
+console.log(typeof(num));
+num="Sanjeev soni";
+console.log(num);
+console.log(typeof(num));
+num=null;
+console.log(num);
+console.log(typeof(num));
+num=undefined;
+console.log(num);
+console.log(typeof(num));
+var sayHello=function (){
+    alert("Hello World");
+}
+//array
+var numberArray=[1,2,3];
+var animals=new Array("cat","dog","mouse","lion");
+//object
+var person={
+    name:"Barack Obama II",
+    age:"51",
+    title:"44th President of the United States"
+}
+
+const sym1=Symbol(4)
+console.log(sym1);
+const sym2=Symbol(4)
+console.log(sym2);
+if(sym1==sym2){
+    console.log("true");
+}
+else{
+    console.log("false")
+}
